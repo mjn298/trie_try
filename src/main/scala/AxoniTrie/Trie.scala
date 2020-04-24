4486f1ca5bbae43337dca3506d2e91fc68ffea09
mjn <mjn298@gmail.com>
2020-04-24 12:26:05 -0400

    updating

--- src/main/scala/AxoniTrie/Trie.scala
+++ src/main/scala/AxoniTrie/Trie.scala
@@ -1,41 +1,67 @@
 package AxoniTrie
 
-sealed trait Trie {
-  def value = Option[String]
-  def insert(key: Int, value: String): Trie = {
-    val bSequence = reverseBinary(key)
-    traverseAndCreate(bSequence, value)
+import scala.annotation.tailrec
+
+sealed trait Trie[+A] {
+  def insert[A](k: Int, s: Option[A]): Trie[A] = {
+    val path = Trie.toBinary(k)
+
+    def loop(p: Seq[Char], n: Trie[A]): Trie[A] = p match {
+      case h :: t if h == '0' => Branch(loop(t, n.left), n.right, n.value)
+      case h :: t if h == '1' => Branch(n.left, loop(t, n.right), n.value)
+      case Nil => n match {
+        case Leaf(_) => Leaf(s)
+        case Branch(l, r, v) => Branch(l, r, s)
+      }
+    }
+
+    loop(path, this)
   }
 
-  // Need to do DF traversal and update/insert
-  def traverseAndCreate(bSeq: Seq[Char], value: String): Node = bSeq match {
-    case x :: Nil =>  if (x == "0") /* update left */ else /* update right */
-    case x::xs => traverseAndCreate(xs, value)
+  def left: Trie[A] = this match {
+    case b: Branch[A] => b.l
+    case _ => Empty
   }
 
-//  def left: Option[Trie] = this match {
-//    case node: Node => Some(node.leftTrie)
-//    case _ : Leaf => None
-//  }
-//
-//  def right: Option[Trie] = this match {
-//    case node: Node => Some(node.rightTrie)
-//    case _: Leaf => None
-//  }
-  def getVal(bSeq: Seq[Char]): Option[String] = bSeq match {
-    case x::xs => if(x == 0) left.getVal(xs) else right.getVal(xs)
-    case x :: Nil => if(x == 0) left.value else right.value
+  def right: Trie[A] = this match {
+    case b: Branch[A] => b.r
+    case _ => Empty
   }
 
-  // Level Order I think
-  def merkleRoot(): String = this match {
-    case l: Leaf => l.value.get().hashCode().toString()
-    case n: Node =>
+  def value: Option[A] = this match {
+    case Branch(_, _, v) => v
+    case Leaf(v) => v
+    case Empty => None
   }
 
-  def reverseBinary(i: Int): Seq[Char] = i.toBinaryString.reverse
+  def fold[A, B](z: B)(lf: Option[A] => B)(b: (B, B) => B): B = this match {
+    case Leaf(v) => lf(v)
+    case Branch(l, r, v: Option[A]) => b(l.fold(lf(v))(lf)(b), r.fold(lf(v))(lf)(b))
+  }
+
+  def getNode(key: Int): Option[Trie[A]] = {
+    val path = Trie.toBinary(key)
+
+    @tailrec
+    def loop(p: Seq[Char], n: Option[Trie[A]]): Option[Trie[A]] = path match {
+      case h :: t if h == '0' => loop(t, n map (t => t.left))
+      case h :: t if h == '1' => loop(t, n map (t => t.right))
+      case Nil => n
+    }
+
+    loop(path, Some(this))
+  }
+
 }
 
+case class Leaf[A](v: Option[A]) extends Trie[A]
 
-case class Node(left: Node, right: Node, value: Option[String]) extends Trie
-case class Leaf(value: Option[String]) extends Trie
+case class Branch[A](l: Trie[A], r: Trie[A], v: Option[A]) extends Trie[A]
+
+case object Empty extends Trie[Nothing]
+
+object Trie {
+  def toBinary(i: Int): Seq[Char] = i.toBinaryString.reverse
+
+  def hashVal(s: String): Int = s.hashCode
+}
